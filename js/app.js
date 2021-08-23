42b8275e00c5898539752dbeaab615d489534a56
Amin Gharibshah <amingharibshah@gmail.com>
2021-08-23 23:36:47 +0430

    add some extra functionality on form

--- js/app.js
+++ js/app.js
@@ -4,11 +4,18 @@
 const valid = "#44C767";
 const invalid = "#ff0000";
 
+// state
+let emailError = false;
+let passwordError = false;
+let confirmPasswordError = false;
+
 // Select elements
+const form = document.getElementById("form");
 const usernameField = document.getElementById("username");
 const emailField = document.getElementById("email");
 const passwordField = document.getElementById("password");
 const confirmPasswordField = document.getElementById("confirm-password");
+const submitBtn = document.getElementById("submit");
 const emailErrorMessage = document.querySelector(".form__error--email");
 const passwordErrorMessage = document.querySelector(".form__error--password");
 const passwordConfirmErrorMessage = document.querySelector(
@@ -33,9 +40,11 @@ emailField.addEventListener("change", function (e) {
   if (emailPattern.test(e.target.value)) {
     e.target.style.borderColor = valid;
     emailErrorMessage.textContent = "";
+    emailError = false;
   } else {
     e.target.style.borderColor = invalid;
     emailErrorMessage.textContent = "Email is not valid";
+    emailError = true;
   }
 });
 
@@ -44,9 +53,11 @@ passwordField.addEventListener("input", function (e) {
   if (passwordPattern.test(e.target.value)) {
     e.target.style.borderColor = valid;
     passwordErrorMessage.textContent = "";
+    passwordError = false;
   } else {
     e.target.style.borderColor = invalid;
     passwordErrorMessage.textContent = "Password must be at least 6 characters";
+    passwordError = true;
   }
 });
 
@@ -56,9 +67,16 @@ confirmPasswordField.addEventListener("input", function (e) {
   else if (e.target.value === passwordField.value) {
     e.target.style.borderColor = valid;
     passwordConfirmErrorMessage.textContent = "";
+    confirmPasswordError = false;
   } else {
     e.target.style.borderColor = invalid;
     passwordConfirmErrorMessage.textContent =
       "Please make sure your passwords match";
+    confirmPasswordError = true;
   }
+});
+
+// submit form
+form.addEventListener("submit", function (e) {
+  if (emailError || passwordError || confirmPasswordError) e.preventDefault();
 });
